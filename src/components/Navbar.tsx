280be3c66bfbd9fe078e087cb317a6a33f3c2656
ruth0107 <ruthvikakula0107@gmail.com>
2025-09-16 14:40:36 +0530

    mobile version 1.0

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,18 +18,15 @@ const Navbar = () => {
     <header className={styles.navbar}>
       <div className={`container ${styles.navContainer}`}>
         <a href="/" className={styles.logo}>
-          {/* 2. Replace the "Rootenzy" text with an img tag */}
           <img src={MainLogo} alt="Rootenzy Logo" />
         </a>
         
-        {/* --- Hamburger Menu Icon --- */}
         <div className={`${styles.hamburger} ${menuOpen ? styles.open : ''}`} onClick={toggleMenu}>
           <span className={`${styles.bar} ${menuOpen ? styles.bar1 : ''}`}></span>
           <span className={`${styles.bar} ${menuOpen ? styles.bar2 : ''}`}></span>
           <span className={`${styles.bar} ${menuOpen ? styles.bar3 : ''}`}></span>
         </div>
         
-        {/* --- Navigation Links --- */}
         <nav className={`${styles.navWrapper} ${menuOpen ? styles.open : ''}`}>
           <ul className={styles.navLinks}>
             <li><Link to="home" smooth={true} duration={500} onClick={closeMenu}>Home</Link></li>
